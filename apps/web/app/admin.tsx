c6f57d5636de546aa665abf4392e5c5f3a3d21e1
SachinM44 <sachin69778@gmail.com>
2025-01-19 13:20:12 +0530

    added react

---
+++ apps/web/app/admin.tsx
@@ -0,0 +1,13 @@
+import Admin from "@repo/ui/admin";
+
+ 
+
+
+ export default function(){
+    return (
+        <div>
+            hello frome admins page 
+            <Admin/>
+        </div>
+    )
+ }
--- packages/ui/src/admin.tsx
+++ packages/ui/src/admin.tsx
@@ -1,4 +1,4 @@
-export default function Admin(){
+export default function  Admin(){
     return (
         <div>
             <header>
